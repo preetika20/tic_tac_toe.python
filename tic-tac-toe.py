17ef7c2f97497a3fa28ad6a2933c7dda120b4675
Preetika Soni <sonipreetika20@gmail.com>
2018-03-31 12:22:07 +0530

    defined 3 functions namely whoGoesFirst(to decide who will go first), playAgain(to replay the game), isWinner(decide winner0

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -32,3 +32,38 @@ def inputPlayerLetter():
     else:
         return['0','X']
 
+def whoGoesFirst():
+    # Randomly choose the player who goes firsr.
+    if random.randint(0,1)==0:
+        return 'computer'
+    else:
+        return 'player'
+
+def playAgain():
+    # this function returns True if the player wants to play again, otherwise it returns false.
+    print('Do you want to play again?(yes or no)')
+    return input().lower().startswith('y')
+
+def makeMove(board, letter,move):
+    board[move] = letter
+
+def isWinner(bo,le):
+    # Given a board and a player's letter , this function returns True if that player has won.
+    #bo=board, le=letter
+
+    return ((bo[7]==le and bo[8]== le and bo==[9]==le)or #across the top
+    (bo[4] == le and bo[5] == le and bo == [6] == le) or  #across the middle)
+    (bo[1] == le and bo[2] == le and bo == [3] == le) or  #across the bottom)
+    (bo[7] == le and bo[4] == le and bo == [1] == le) or  #down the left side)
+    (bo[8] == le and bo[5] == le and bo == [2] == le) or  #down the middle)
+    (bo[9] == le and bo[6] == le and bo == [3] == le) or  #down the right side)
+    (bo[7] == le and bo[5] == le and bo == [3] == le) or  #diagnol)
+    (bo[9] == le and bo[5] == le and bo == [1] == le) )or  #diagnol)
+
+
+
+
+
+
+
+

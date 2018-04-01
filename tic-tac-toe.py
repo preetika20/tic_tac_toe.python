64be40c38675830c68a19752ce819fd902c47e9f
Preetika Soni <sonipreetika20@gmail.com>
2018-04-01 15:46:48 +0530

    defined 4 functions namely getBoardCopy(to duplicate board list), isSpaceFree, getPlayerMove(to get player's move),chooseRandomMoveFromList (to get a random number from list).

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -58,12 +58,39 @@ def isWinner(bo,le):
     (bo[8] == le and bo[5] == le and bo == [2] == le) or  #down the middle)
     (bo[9] == le and bo[6] == le and bo == [3] == le) or  #down the right side)
     (bo[7] == le and bo[5] == le and bo == [3] == le) or  #diagnol)
-    (bo[9] == le and bo[5] == le and bo == [1] == le) )or  #diagnol)
+    (bo[9] == le and bo[5] == le and bo == [1] == le) ) #diagnol)
 
 
+def getBoardCopy(board):
+    # Make a duplicate of the board list and return it to the duplicate.
+    dupeBoard=[]
 
+    for i in board:
+        dupeBoard.append(i)
 
+    return dupeBoard
 
+def isSpaceFree(board,move):
+    # Return true if the passed move is free on the passed board.
+    return board[move] == ' '
 
+def getPlayerMove(board):
+    # Let the player type in their move.
+    move =''
+    while move not in ' 1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board,move)):
+        print('What is your next move? (1-9)' )
+        move=input()
+    return int(move)
 
+def chooseRandomMoveFromList(board, movesList):
+    # Returns a valid move from the passed list on the passed board.
+    # Returns None if there is no valid move.
+    possibleMoves = []
+    for i in movesList:
+        if isSpaceFree(board, i):
+            possibleMoves.append(i)
 
+    if len(possibleMoves) != 0:
+        return random.choice(possibleMoves)
+    else:
+        return None
